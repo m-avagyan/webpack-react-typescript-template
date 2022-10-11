bfe29b508dd1a53ebadaa3c8d99cfc22e2cf4eb9
m-avagyan <martikavagyan1@gmail.com>
2022-10-11 21:55:31 +0400

    wip: eslint and prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'jsx-a11y', 'react-hooks', 'prettier', '@typescript-eslint'],
+    rules: {
+        'no-console': ['error', { allow: ['info', 'groupCollapsed', 'groupEnd', 'error'] }],
+        'prettier/prettier': ['error', { endOfLine: 'auto' }],
+        'react/react-in-jsx-scope': 0,
+        'no-use-before-define': ['error', 'nofunc'],
+        '@typescript-eslint/no-use-before-define': ['error', 'nofunc'],
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    proseWrap: 'always',
+    quoteProps: 'as-needed',
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    useTabs: false,
+    tabWidth: 4,
+    bracketSpacing: true,
+    printWidth: 120,
+    arrowParens: 'always',
+    importOrderSeparation: true,
+    experimentalBabelParserPluginsList: ['jsx', 'tsx'],
+};
