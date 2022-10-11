e945ae06aefd4ea48c4325260f379dfacfbee548
m-avagyan <martikavagyan1@gmail.com>
2022-10-11 22:23:50 +0400

    wip: react router migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+
 import { useTranslation } from 'react-i18next';
+
+import Paths from './constants/path';
+
+import routes from './routes';
+import { Link } from 'react-router-dom';
 
 function App() {
     const { t } = useTranslation();
 
-    return <div>{t('home.description')}</div>;
+    return (
+        <div>
+            {t('home.description')}
+
+            <Link to={Paths.TEST_PAGE}>Test Page</Link>
+
+            <Routes>
+                {routes.map(({ name, path, Component }, index) => (
+                    <Route
+                        key={name + index}
+                        path={path}
+                        element={
+                            <Suspense fallback={null}>
+                                <Component />
+                            </Suspense>
+                        }
+                    />
+                ))}
+
+                <Route path="*" element={<Navigate replace to={Paths.HOME} />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Loader from './components/loader/Loader';
-
 import './i18next';
 
-const App = lazy(() => import(/* webpackChunkName: "app" */ './App'));
+const App = lazy(() => import(/* webpackChunkName: "app-context" */ './App'));
 
 ReactDOM.render(
-    <Suspense fallback={<Loader />}>
-        <App />
-    </Suspense>,
+    <Router>
+        <Suspense fallback={<Loader />}>
+            <App />
+        </Suspense>
+    </Router>,
     document.getElementById('root')
 );
