b722acca97413e114e3243fc80a19a09ea0b334d
m-avagyan <martikavagyan1@gmail.com>
2023-11-24 21:38:26 +0400

    feat: update react and react-router-dom versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { Suspense } from 'react';
-import { Navigate, Route, Routes } from 'react-router';
-
 import { useTranslation } from 'react-i18next';
+import { Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import Paths from './constants/path';
-
 import routes from './routes';
-import { Link } from 'react-router-dom';
 
 function App() {
     const { t } = useTranslation();
@@ -17,21 +15,15 @@ function App() {
 
             <Link to={Paths.TEST_PAGE}>Test Page</Link>
 
-            <Routes>
+            <Switch>
                 {routes.map(({ name, path, Component }, index) => (
-                    <Route
-                        key={name + index}
-                        path={path}
-                        element={
-                            <Suspense fallback={null}>
-                                <Component />
-                            </Suspense>
-                        }
-                    />
+                    <Route key={name + index} path={path} exact>
+                        <Suspense fallback={null}>
+                            <Component />
+                        </Suspense>
+                    </Route>
                 ))}
-
-                <Route path="*" element={<Navigate replace to={Paths.HOME} />} />
-            </Routes>
+            </Switch>
         </div>
     );
 }
