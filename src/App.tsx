4ab7292aced541802d8de2555882af3ff4e3c26e
m-avagyan <martikavagyan1@gmail.com>
2024-02-29 16:05:09 +0400

    feat: add styled example

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,58 @@ import { useTranslation } from 'react-i18next';
 import { Route, Switch } from 'react-router';
 import { Link } from 'react-router-dom';
 
+import styled from 'styled-components';
+
 import Loader from './components/loader';
 import Paths from './constants/path';
 import routes from './routes';
+
+const StyledDescription = styled.div`
+    text-align: center;
+    padding: 40px;
+    color: #000;
+    font-size: 20px;
+    line-height: 28px;
+    font-weight: bold;
+`;
+
+const StyledContainer = styled.div`
+    width: 100%;
+    padding: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: gray;
+    border-radius: 20px;
+`;
+
+const StyledLink = styled(Link)`
+    color: blue;
+    font-size: 20px;
+    line-height: 28px;
+    font-weight: bold;
+`;
 
 function App() {
     const { t } = useTranslation();
 
     return (
         <div>
-            {t('home.description')}
+            <StyledDescription>{t('home.description')}</StyledDescription>
 
-            <Link to={Paths.TEST_PAGE}>Test Page</Link>
+            <StyledLink to={Paths.TEST_PAGE}>Test Page</StyledLink>
 
-            <Switch>
-                {routes.map(({ name, path, Component }, index) => (
-                    <Route key={name + index} path={path} exact>
-                        <Suspense fallback={<Loader />}>
-                            <Component />
-                        </Suspense>
-                    </Route>
-                ))}
-            </Switch>
+            <StyledContainer>
+                <Switch>
+                    {routes.map(({ name, path, Component }, index) => (
+                        <Route key={name + index} path={path} exact>
+                            <Suspense fallback={<Loader />}>
+                                <Component />
+                            </Suspense>
+                        </Route>
+                    ))}
+                </Switch>
+            </StyledContainer>
         </div>
     );
 }
